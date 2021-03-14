3fd8a89bcf41cd901ed70012e066b63bd2816686
tales <talesdenorie@gmail.com>
2021-03-15 11:16:22 +1300

    has a nice login page, crappy home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import SignIn from './SignIn';
+import Home from "./Home";
+import { Switch, Route, useLocation } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import { CssBaseline } from "@material-ui/core";
+
 
 function App() {
+  const history = useHistory();
+  let location = useLocation();
+  
+  function navigateTo(newPath) {
+    console.log(location.pathname + " vs " + newPath);
+    //prevents unnecessary push to history
+    if (location.pathname != newPath) {
+      console.log("pushed " + newPath);
+      history.push(newPath);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Testing
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Switch>
+        <Route path="/login">
+          <SignIn propPassedNavTo={navigateTo} />
+        </Route>
+        <Route path="/">
+          <Home propPassedNavTo={navigateTo} />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
