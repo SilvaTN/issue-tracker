3f7111ac797c501e8ece94228933683aa14413dc
tales <talesdenorie@gmail.com>
2021-03-22 19:40:06 +1300

    modularised styling of body into theme.js

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,50 +1,10 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import { Paper } from "@material-ui/core";
-
-const DRAWER_WIDTH = 240;
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    },
-    mainContent: {
-      paddingTop: theme.spacing(10),
-      paddingRight: theme.spacing(9.2),
-      paddingBottom: theme.spacing(9),
-      paddingLeft: theme.spacing(9.2),
-      minHeight: "100vh",
-      transition: theme.transitions.create(['margin'], {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      [theme.breakpoints.down("xs")]: {
-        //whatever is in here will be applied when the theme's breakpoint is extra small or above.
-        paddingLeft: theme.spacing(8.4),
-      }
-    },
-    mainContentShift: {
-      marginLeft: DRAWER_WIDTH,
-      paddingTop: theme.spacing(10),
-      paddingRight: theme.spacing(3),
-      paddingBottom: theme.spacing(9),
-      paddingLeft: theme.spacing(3),
-      minHeight: "100vh",
-      [theme.breakpoints.down("xs")]: {
-        //whatever is in here will be applied when the theme's breakpoint is extra small or above.
-        paddingLeft: theme.spacing(2.3),
-      }
-    },
-}));
+import useStylesForMain from "./theme";
 
 const Home = ( { open } ) => {
-  const classes = useStyles();
+  // const classes = useStyles();
+  const classes = useStylesForMain();
 
   return (
     <div className={classes.root}>
